35214aff200e75dbf2ed9d21041bbeec267e2b55
radiaku <aku.radiaku@gmail.com>
2024-10-02 17:15:05 +0700

    fixing template preview

--- lua/radia/plugins/telescope.lua
+++ lua/radia/plugins/telescope.lua
@@ -29,6 +29,16 @@ return {
 
 		telescope.setup({
 			defaults = {
+				-- layout_strategy = "horizontal",
+				-- layout_config = {
+				-- 	horizontal = {
+				-- 		prompt_position = "top",
+				-- 		width = { padding = 0 },
+				-- 		height = { padding = 0 },
+				-- 		preview_width = 0.5,
+				-- 	},
+				-- },
+				-- sorting_strategy = "ascending",
 				path_display = { "truncate" },
 				mappings = {
 					i = {
