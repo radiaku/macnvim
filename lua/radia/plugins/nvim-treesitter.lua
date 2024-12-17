614302503dd2417f4a434f4f737c5e3734c0ff70
radiaku <aku.radiaku@gmail.com>
2024-12-17 13:55:43 +0700

    fixing tree sitter

--- lua/radia/plugins/nvim-treesitter.lua
+++ lua/radia/plugins/nvim-treesitter.lua
@@ -10,24 +10,31 @@ return {
 			-- import nvim-treesitter plugin
 			local treesitter = require("nvim-treesitter.configs")
 
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+			-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+			-- diagnostics = { disable = { 'missing-fields' } },
 
 			-- configure treesitter
 			treesitter.setup({
-        -- enable syntax highlighting
+				-- enable syntax highlighting
 				highlight = {
 					enable = true,
 				},
 				-- enable indentation
-				indent = { enable = true},
+				indent = { enable = true },
 				-- enable autotagging (w/ nvim-ts-autotag plugin)
 				autotag = {
 					enable = false,
 				},
+
 				disable = function()
 					return string.len(table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "")) > 500000
 				end,
+
+				context_commentstring = {
+					enable = false,
+					enable_autocmd = false,
+				},
+
 				-- ensure these language parsers are installed
 				ensure_installed = {
 					"json",
