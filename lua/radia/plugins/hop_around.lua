2521c50304bccdccd63cee0b8ba5e56089848a90
radiaku <aku.radiaku@gmail.com>
2024-12-23 22:43:52 +0700

    clean hop

--- lua/radia/plugins/hop_around.lua
+++ lua/radia/plugins/hop_around.lua
@@ -1,8 +1,6 @@
 return {
 	"smoka7/hop.nvim",
 	config = function()
-		-- you can configure Hop the way you like here; see :h hop-config
 		require("hop").setup({ keys = "etovxqpdygfblzhckisuran", term_seq_bias = 0.5 })
-		-- vim.api.nvim_set_keymap("n", "f", "<cmd>lua require'hop'.hint_char1()<cr>", {})
 	end,
 }
