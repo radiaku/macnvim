c9db7fa78333c526c8dc6d0ee2346c31e568fa06
radiaku <aku.radiaku@gmail.com>
2025-02-23 13:52:42 +0700

    fixing bling for suport tab

--- lua/radia/plugins/blink.lua
+++ lua/radia/plugins/blink.lua
@@ -12,14 +12,19 @@ return {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "accept", "fallback" },
 
-			["<Tab>"] = { "snippet_forward", "fallback" },
-			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			-- ["<Tab>"] = { "snippet_forward", "fallback" },
+			-- ["<S-Tab>"] = { "snippet_backward", "fallback" },
 
 			["<Up>"] = { "select_prev", "fallback" },
 			["<Down>"] = { "select_next", "fallback" },
+
 			["<C-p>"] = { "select_prev", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
+
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
 
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
@@ -49,7 +54,7 @@ return {
 		},
 		sources = {
 			default = { "lsp", "snippets", "path", "buffer" },
-			min_keyword_length = 3,
+			min_keyword_length = 2,
 		},
 	},
 }
