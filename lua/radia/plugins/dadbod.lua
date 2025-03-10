ea3048a190e5b98e7606fb3db3fdfd2d6fce3207
radiaku <aku.radiaku@gmail.com>
2025-03-11 02:23:52 +0700

    fixing query dadbob

--- lua/radia/plugins/dadbod.lua
+++ lua/radia/plugins/dadbod.lua
@@ -13,6 +13,6 @@ return {
 	init = function()
 		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
-		vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/querydadbod/"
+		vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/querydadbod"
 	end,
 }
