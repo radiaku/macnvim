9bd9eb77e4f082de368d49ad8f4cffd1b732e788
radiaku <aku.radiaku@gmail.com>
2025-03-17 21:14:05 +0700

    on large file 4 mb or more. disable everything

---
+++ lua/radia/plugins/large_file.lua
@@ -0,0 +1,9 @@
+return {
+	"mireq/large_file",
+	config = function()
+		require("large_file").setup()
+	end,
+}
+-- https://www.reddit.com/r/neovim/comments/190ntm3/comment/kgpx3rq/
+-- https://github.com/mireq/neovim-config/blob/master/lua/core/largefile.lua
+-- https://github.com/mireq/large_file
