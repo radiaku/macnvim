e2084acf096f5aa556d38f21a2bd7504667acb43
radiaku <aku.radiaku@gmail.com>
2025-03-18 17:40:15 +0700

    fixing last

--- lua/radia/last.lua
+++ lua/radia/last.lua
@@ -54,7 +54,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		-- vim.o.setlocal = true
     vim.o.autoindent = true
 		vim.o.tabstop = 4
-		vim.o.expandtab = true
+		vim.o.expandtab = false -- false to tab insteat
 		vim.o.softtabstop = 4
 		vim.o.shiftwidth = 4
 		vim.o.smartindent = true
