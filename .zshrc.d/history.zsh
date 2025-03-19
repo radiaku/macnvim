7644e9b8f42ea2cea4a97d087f0573de3052aec3
radiaku <aku.radiaku@gmail.com>
2025-03-19 14:10:16 +0700

    history zsh

--- .zshrc.d/history.zsh
+++ .zshrc.d/history.zsh
@@ -4,3 +4,6 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTFILE=~/.cache/zsh/history
 
+setopt APPEND_HISTORY  # Append to the history file, don't overwrite
+setopt HIST_IGNORE_DUPS  # Ignore duplicate commands
+
