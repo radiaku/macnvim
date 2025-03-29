55ce4257c85ebf86e39016c43d14d9e349ca0257
radiaku <aku.radiaku@gmail.com>
2025-03-30 02:33:54 +0700

    remove x from insert mode telescope

--- lua/radia/plugins/telescope.lua
+++ lua/radia/plugins/telescope.lua
@@ -118,7 +118,6 @@ return {
 						["<C-k>"] = actions.move_selection_previous,
 						["<C-j>"] = actions.move_selection_next,
 						["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-						["x"] = actions.toggle_selection + actions.move_selection_better,
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 						["<C-Q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
