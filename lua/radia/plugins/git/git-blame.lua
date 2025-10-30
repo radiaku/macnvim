1aa8ccad9597374a4262b8f678e2d944d7aff389
radiaku <aku.radiaku@gmail.com>
2025-10-30 20:48:42 +0700

    fixing git blame with formatted date

--- lua/radia/plugins/git/git-blame.lua
+++ lua/radia/plugins/git/git-blame.lua
@@ -3,5 +3,6 @@ return {
   config = function()
     vim.g.gitblame_enabled = 1
     vim.g.gitblame_message_template = '<author> • <date> • <summary>'
+    vim.g.gitblame_date_format = '%Y-%b-%d %H:%M'  -- e.g. 2025-10-30 14:23
   end,
 }
