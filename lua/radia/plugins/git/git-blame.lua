5b20cab8e6ba18fef0d2b3dd8bdeb3b05f8bdb75
radiaku <aku.radiaku@gmail.com>
2025-10-30 20:49:48 +0700

    make git blame start with toggle off

--- lua/radia/plugins/git/git-blame.lua
+++ lua/radia/plugins/git/git-blame.lua
@@ -1,7 +1,7 @@
 return {
   "f-person/git-blame.nvim",
   config = function()
-    vim.g.gitblame_enabled = 1
+    vim.g.gitblame_enabled = 0
     vim.g.gitblame_message_template = '<author> • <date> • <summary>'
     vim.g.gitblame_date_format = '%Y-%b-%d %H:%M'  -- e.g. 2025-10-30 14:23
   end,
